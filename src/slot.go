fa810a15c73789a9a8ce80cd686b4fd538980bff
smit.bhoraniya_ftc <smit.bhoraniya_ftc@external.swiggy.in>
2024-02-01 10:40:03 +0530

    Implemented park and unpark methods in slot

--- src/slot.go
+++ src/slot.go
@@ -1,8 +1,13 @@
 package src
 
+import (
+	"errors"
+	"os/exec"
+)
+
 type Slot struct {
-	car Car
-	id  string
+	car    Car
+	ticket string
 }
 
 func NewSlot() Slot {
@@ -12,4 +17,33 @@ func NewSlot() Slot {
 func (s Slot) isFree() bool {
 	var slot Slot
 	return s == slot
+}
+
+func (s *Slot) park(car Car) (string, error) {
+	if !s.isFree() {
+		return "", errors.New("Slot is occupied.")
+	}
+
+	s.car = car
+	newUUID, _ := exec.Command("uuidgen").Output()
+	s.ticket = string(newUUID)
+	return s.ticket, nil
+}
+
+func (s Slot) isValidTicket(ticket string) bool {
+	return s.ticket == ticket
+}
+
+func (s *Slot) unPark(ticket string) (Car, error) {
+	if s.isFree() {
+		return Car{}, errors.New("Slot is empty.")
+	}
+	if !s.isValidTicket(ticket) {
+		return Car{}, errors.New("Invalid ticket.")
+	}
+
+	res := s.car
+	s.car = Car{}
+	s.ticket = ""
+	return res, nil
 }
--- src/slot_test.go
+++ src/slot_test.go
@@ -9,3 +9,57 @@ func TestIsSlotFree(t *testing.T) {
 		t.Errorf("Slot should be free, when created.")
 	}
 }
+
+func TestParkCar(t *testing.T) {
+	car := NewCar("Ab", BLACK)
+	slot := NewSlot()
+
+	_, err := slot.park(car)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+}
+
+func TestIsValidParkingTicket(t *testing.T) {
+	car := NewCar("Ab", BLACK)
+	slot := NewSlot()
+
+	ticket, _ := slot.park(car)
+	if !slot.isValidTicket(ticket) {
+		t.Errorf("Ticket is not valid.")
+	}
+}
+
+func TestUnparkCar(t *testing.T) {
+	car := NewCar("Ab", BLACK)
+	slot := NewSlot()
+
+	ticket, _ := slot.park(car)
+	unparkedCar, _ := slot.unPark(ticket)
+
+	if car != unparkedCar {
+		t.Errorf("Invalid car unparked.")
+	}
+}
+
+func TestUnparkCarWithEmptySlot(t *testing.T) {
+	slot := NewSlot()
+
+	_, err := slot.unPark("abc")
+
+	if err == nil {
+		t.Errorf("Couldn't unpark empty slot.")
+	}
+}
+
+func TestUnparkCarWithInvalidTicket(t *testing.T) {
+	car := NewCar("Ab", BLACK)
+	slot := NewSlot()
+
+	slot.park(car)
+	_, err := slot.unPark("abc")
+
+	if err == nil {
+		t.Errorf("Couldn't unpark car using invalid ticket.")
+	}
+}
